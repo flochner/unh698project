4f0db54a5f2158fe31b748e179125a3eef015e28
flochner <flochner@gmail.com>
2017-05-15 00:26:21 -0400

    added Archer html, updated main html

--- projectWeb.py
+++ projectWeb.py
@@ -10,7 +10,7 @@ def home():
     return render_template('main.html')
 
 @app.route('/SpeedRacer')
-def fakeDatabase():
+def fakeSpeed_db():
     f = (
          {'name':'Speed', 'castMember':'18-year-old professional racer'},
          {'name':'Spritle', 'castMember':'Speed\'s little brother'},
@@ -24,6 +24,26 @@ def fakeDatabase():
          {'name':'Sparky', 'castMember':'Mach 5 mechanic'},
         )
     return render_template('SpeedRacer/main.html', family=f, others=o)
+
+@app.route('/Archer')
+def fakeArcher_db():
+    a = (
+         {'name':'Speed', 'castMember':'18-year-old professional racer'},
+         {'name':'Spritle', 'castMember':'Speed\'s little brother'},
+         {'name':'Pops', 'castMember':'Speed\'s father'},
+         {'name':'Mrs. Racer', 'castMember':'Speed\'s mother'},
+        )
+    o = (
+         {'name':'Trixie', 'castMember':'Speed\'s girlfriend'},
+         {'name':'Racer X', 'castMember':'Masked racer, and secretly, Speed\'s older brother'},
+         {'name':'Chim-Chim', 'castMember':'Spritle\'s chimpanzee'},
+         {'name':'Sparky', 'castMember':'Mach 5 mechanic'},
+        )
+    return render_template('Archer/main.html', agents=a, others=o)
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
