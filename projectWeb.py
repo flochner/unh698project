cd14a995ec65ab201951aa9c19fcabae7ed1ab30
flochner <flochner@gmail.com>
2017-05-15 11:11:41 -0400

    Finished Archer descriptions

--- projectWeb.py
+++ projectWeb.py
@@ -29,16 +29,17 @@ def fakeSpeed_db():
 @app.route('/Archer')
 def fakeArcher_db():
     a = (
-         {'name':'Speed', 'castMember':'18-year-old professional racer'},
-         {'name':'Spritle', 'castMember':'Speed\'s little brother'},
-         {'name':'Pops', 'castMember':'Speed\'s father'},
-         {'name':'Mrs. Racer', 'castMember':'Speed\'s mother'},
+         {'name':'Sterling Malory Archer', 'castMember':'Codename: Duchess. ISIS Field Agent. Considered the world\'s most dangerous secret agent.'},
+         {'name':'Malory Duchess Archer', 'castMember':'ISIS Director. Streling Archer\'s mother.'},
+         {'name':'Lana Anthony Kane', 'castMember':'ISIS Field Agent. ISIS\' top female agent and Archer\'s one-time girfriend.'},
         )
     o = (
-         {'name':'Trixie', 'castMember':'Speed\'s girlfriend'},
-         {'name':'Racer X', 'castMember':'Masked racer, and secretly, Speed\'s older brother'},
-         {'name':'Chim-Chim', 'castMember':'Spritle\'s chimpanzee'},
-         {'name':'Sparky', 'castMember':'Mach 5 mechanic'},
+         {'name':'Cyril Figgis', 'castMember':'ISIS Comptroller.  Also one-time boyfriend of Lana.'},
+         {'name':'Cheryl/Carol/Cherlene (Gimble) Tunt', 'castMember':'Malory\'s mentally unstable assistant.'},
+         {'name':'Pam Poovey', 'castMember':'ISIS Human Resources Director. Fond of bear claws.'},
+         {'name':'Dr. Algernop Krieger', 'castMember':'Head of ISIS Applied Research. Questionable morals at best.'},
+         {'name':'Arthur Henry Woodhouse', 'castMember':'Sterling\'s long-suffering British valet.'},
+         {'name':'', 'castMember':''},
         )
     return render_template('Archer/main.html', agents=a, others=o)
 
